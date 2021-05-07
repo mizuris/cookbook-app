160297592ca77c78f232e91f3deec8c3a92ae90a
Piotr_Bator <pbator94@gmail.com>
2021-05-07 09:09:16 +0200

    Change project structure and purpose

--- src/App.js
+++ src/App.js
@@ -1,49 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.scss";
-import axios from "axios";
-import RecipesList from "./components/Recipe/RecipesList";
+
 
 function App() {
-  const [recipeList, setRecipeList] = useState([]);
-  const [recipe, setRecipe] = useState({
-    query: "chicken",
-    number: 1,
-  });
 
-  const url = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&query=${recipe.query}&number=${recipe.number}`;
-
-  const getRecipe = async () => {
-    if (recipe.query !== "") {
-      const queryResult = await axios.get(url);
-      setRecipeList(queryResult.data.results);
-    }
-  };
 
   return (
     <div className="App">
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          getRecipe();
-        }}
-      >
-        <input
-          type="text"
-          value={recipe.query}
-          placeholder="Search recipe"
-          onChange={(e) => setRecipe({ ...recipe, query: e.target.value })}
-        />
-        <input
-          type="number"
-          value={recipe.number}
-          min="1"
-          max="100"
-          onChange={(e) => setRecipe({ ...recipe, number: e.target.value })}
-        />
-        <input type="submit" value="Find recipe" />
-      </form>
 
-      {recipeList.length ? <RecipesList recipeList={recipeList} /> : ""}
     </div>
   );
 }
---
+++ src/components/GeneratePlan/GeneratePlan.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function GeneratePlan() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default GeneratePlan
