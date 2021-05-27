fb194cbc78900044fc1959a9dd77e99b1b09ecf5
Piotr_Bator <pbator94@gmail.com>
2021-05-27 12:41:53 +0200

    Set test fetch function to get details of recipe

--- src/components/Recipe/Recipe.js
+++ src/components/Recipe/Recipe.js
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function Recipe({ recipe }) {
-  const { title, image } = recipe;
+  const { title, image, id } = recipe;
+  const [recipeDetails, setRecipeDetails] = useState(null);
+
+  useEffect(() => {
+    const getRecipeId = async () => {
+      const recipeDetailsResult = await fetch(
+        `https://api.spoonacular.com/recipes/${id}/information?apiKey=${process.env.REACT_APP_API_KEY}`
+      );
+      recipeDetailsResult.json().then((data) => setRecipeDetails(data));
+    };
+    getRecipeId();
+  }, [id]);
+
   return (
     <div>
       <img src={image} alt={title} />
       <p>{title}</p>
+      {recipeDetails ? (
+        <>
+          <p>{recipeDetails.readyInMinutes} - minutes</p>
+          <p>{recipeDetails.sourceUrl} - url</p>
+          <p>{recipeDetails.summary} - summary</p>
+        </>
+      ) : (
+        ""
+      )}
     </div>
   );
 }
