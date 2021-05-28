0b2b5c065aec3bf8e1d3a63c215630d9d54d7452
Piotr_Bator <pbator94@gmail.com>
2021-05-28 10:22:50 +0200

    Add material/ui Icons. Set grid display of Recipe component

--- src/components/Recipe/Recipe.js
+++ src/components/Recipe/Recipe.js
@@ -1,13 +1,68 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import ButtonBase from "@material-ui/core/ButtonBase";
+import Typography from "@material-ui/core/Typography";
+import FavoriteBorderOutlinedIcon from "@material-ui/icons/FavoriteBorderOutlined";
+import AccessTimeIcon from "@material-ui/icons/AccessTime";
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    padding: theme.spacing(2),
+    margin: "auto",
+    maxWidth: 500,
+  },
+  image: {
+    width: 128,
+    height: 128,
+  },
+  img: {
+    margin: "auto",
+    display: "block",
+    maxWidth: "100%",
+    maxHeight: "100%",
+  },
+}));
 
 function Recipe({ recipe }) {
-  const { title, image, sourceUrl } = recipe;
+  const classes = useStyles();
+  const { title, image, sourceUrl, readyInMinutes } = recipe;
 
   return (
-    <div>
-      <img src={image} alt={title} />
-      <p>{title}</p>
-      <a href={sourceUrl}>Get details</a>
+    <div className="recipe-container">
+      <Paper className={classes.paper}>
+        <Grid container spacing={3}>
+          <Grid item>
+            <ButtonBase className={classes.image}>
+              <img className={classes.img} alt={title} src={image} />
+            </ButtonBase>
+          </Grid>
+          <Grid item xs={12} sm container>
+            <Grid item xs container direction="column" spacing={2}>
+              <Grid item xs>
+                <Typography gutterBottom variant="subtitle1">
+                  {title}
+                </Typography>
+                <Typography variant="body2" color="textSecondary">
+                  <a href={sourceUrl}>Recipe source</a>
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Typography variant="body2">
+                  <FavoriteBorderOutlinedIcon style={{ cursor: "pointer" }} />
+                </Typography>
+              </Grid>
+            </Grid>
+            <Grid item>
+              <Typography variant="subtitle1">
+                <AccessTimeIcon />
+                {readyInMinutes}
+              </Typography>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Paper>
     </div>
   );
 }
